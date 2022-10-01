7ad151e6e2722509a210546f269af20a40cf40ce
joaomarcelowapstore <joao.gomes@wapstore.com.br>
2022-10-01 12:48:15 -0300

    Add first controller and GET route with mocked data for listing visualization.

---
+++ app/Http/Controllers/ToolController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Response;
+
+class ToolController extends Controller
+{
+    public function index()
+    {
+        return [
+            "id" => 1,
+            "title" => "Notion",
+            "link"   => "http://.notion.so",
+            "description" => "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ",
+            "tags" => [
+                "organization",
+                "planning",
+                "collaboration",
+                "writing",
+                "calendar"
+            ]
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/teste', ['uses' => 'ToolController@index']);
